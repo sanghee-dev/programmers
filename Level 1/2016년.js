7baec8502715ac24e9468d2f589a5d7523e1b264
leeedev <leeesangheee@gmail.com>
2021-04-25 23:12:45 +0900

    Create 2016년.js

---
+++ Level 1/2016년.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 24;
+
+function solution(a, b) {
+  const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const day = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+  const count = days.slice(0, a).reduce((prev, acc) => prev + acc) + b;
+  return day[count % 7];
+}
+
+console.log(solution(a, b));
