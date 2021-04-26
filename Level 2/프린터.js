c7620206666f37c7b9630ef172a314d398e45296
leeedev <leeesangheee@gmail.com>
2021-04-27 01:00:19 +0900

    Update 프린터.js

--- Level 2/프린터.js
+++ Level 2/프린터.js
@@ -2,7 +2,10 @@ const priorities = [2, 1, 3, 2];
 const location = 2;
 
 function solution(priorities, location) {
-  const list = priorities.map((v, i) => ({ my: i === location, priority: v }));
+  const list = priorities.map((v, i) => ({
+    isLocation: i === location,
+    priority: v,
+  }));
   let count = 0;
   while (true) {
     let cur = list.shift();
@@ -10,7 +13,7 @@ function solution(priorities, location) {
       list.push(cur);
     } else {
       count++;
-      if (cur.my) return count;
+      if (cur.isLocation) return count;
     }
   }
 }
