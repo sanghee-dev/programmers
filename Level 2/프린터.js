8244ce63c7ce0098de4bb6a96031cea1b5248126
leeedev <leeesangheee@gmail.com>
2021-04-26 21:55:02 +0900

    Create 프린터.js

---
+++ Level 2/프린터.js
@@ -0,0 +1,18 @@
+const priorities = [2, 1, 3, 2];
+const location = 2;
+
+function solution(priorities, location) {
+  const list = priorities.map((v, i) => ({ my: i === location, priority: v }));
+  let count = 0;
+  while (true) {
+    let cur = list.shift();
+    if (list.some((v) => cur.priority < v.priority)) {
+      list.push(cur);
+    } else {
+      count++;
+      if (cur.my) return count;
+    }
+  }
+}
+
+console.log(solution(priorities, location));
