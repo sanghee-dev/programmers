1fce840e59e85d542b9b643b4992b8f75ba8c285
leeedev <leeesangheee@gmail.com>
2021-04-28 00:52:31 +0900

    Update 124 나라의 숫자.js

--- Level 2/124 나라의 숫자.js
+++ Level 2/124 나라의 숫자.js
@@ -1,10 +1,10 @@
 let n = 10;
 
 function solution(n) {
-  const arr = [1, 2, 4];
+  const arr = [4, 1, 2];
   let answer = "";
   while (n) {
-    answer = arr[(n - 1) % 3] + answer;
+    answer = arr[n % 3] + answer;
     n = n % 3 === 0 ? n / 3 - 1 : Math.floor(n / 3);
   }
   return answer;
