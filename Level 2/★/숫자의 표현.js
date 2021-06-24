706f11707e33444e7edfe2115759ae4d82a28af5
leeedev <leeesangheee@gmail.com>
2021-06-24 18:18:51 +0900

    숫자의 표현

--- Level 2/★/숫자의 표현.js
+++ Level 2/★/숫자의 표현.js
@@ -2,7 +2,7 @@ const n = 15;
 
 function solution(n) {
   let count = 0;
-  for (let i = 1; i * i < n * 2; i++) {
+  for (let i = 1; i <= Math.sqrt(n * 2); i++) {
     if ((n * 2) % i === 0 && (i + (n * 2) / i) % 2 === 1) count++;
   }
   return count;
