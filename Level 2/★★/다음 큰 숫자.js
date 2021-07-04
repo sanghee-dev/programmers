65313d208a8ef3586da7602c53108a4fe302334b
leeedev <leeesangheee@gmail.com>
2021-07-04 11:43:11 +0900

    다음 큰 숫자

--- Level 2/★★/다음 큰 숫자.js
+++ Level 2/★★/다음 큰 숫자.js
@@ -1,12 +1,5 @@
 let n = 78;
 
-function solution(n) {
-  let countOne = n.toString(2).match(/1/g).length;
-  while (n++) {
-    if (countOne === n.toString(2).match(/1/g).length) return n;
-  }
-}
-
 function solution(n, a = n + 1) {
   return n.toString(2).match(/1/g).length === a.toString(2).match(/1/g).length
     ? a
